1e8e3deb787b2168c4de102cb3365bdc63f6f995
sdress <sarah.dresser1@gmail.com>
2022-03-22 23:04:53 -0700

    adding base files, midway

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
-# don't forget to import flask & pymysql in shell
-
 from flask_app import app
-# from flask_app.controllers import whatever controllers
+from flask_app.controllers import 
 
 if __name__ == "__main__":
     app.run(debug=True)
